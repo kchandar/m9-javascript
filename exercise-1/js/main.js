c52f305cc2a49a1ba51b9a9f9359d61df1ff5b57
Keertana Chandar <kchandar@uw.edu>
2016-10-11 10:16:46 -0700

    class exercises

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -4,17 +4,37 @@
 var str = "this is a sentence";
 
 // Use the length property to see how many characters are in your `str` variable
+var strLength = str.length;
+console.log(strLength);
 
 // Use the `toUpperCase` method to create a new variable `STR` that has the `str` value, but all UPPPERCASE.
+var STR = str.toUpperCase();
+console.log(STR);
 
 // Use the "split" method create a new variable `words` that is an array of the component words of `str`
+var words = str.split(" ");
+console.log(words);
 
 // Use the length property to see how many words are in your `words` array
+var wordsLength = words.length;
+console.log(wordsLength);
 
 // Write a function that returns the number of words in a string
+var numWords = function(string) {
+	return string.split(" ").length;
+}
 
 // Pass a string to your function to determine that it works
+console.log(numWords(str));
 
 // Write a function to test if a string contains a word, and returns a boolean value.
+var hasWord = function(string, word) {
+	if(string.includes(word)) {
+		return true;
+	}
+	return false;
+}
 
 // Pass a phrase and a string to your function to determine that it works
+console.log(hasWord(str, "this"));
+console.log(hasWord("", "hi"));
--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -2,23 +2,34 @@
 
 // Array to work with
 var arr1 = [1,2,3,4,5];
+console.log(arr1);
 
 // Write a function to subtract one from a value
+var subtractOne = function(x) {
+	return x - 1;
+}
 
 
 // Pass each value to the subtractOne function by using the map functionality
+var minOne = arr1.map(subtractOne);
+console.log(minOne);
 
 
 // Create a array equal to arr1 times two by using the inline "map" funcitonality
-
+var mulTwo = arr1.map(function(num) {
+	return num * 2;
+})
+console.log(mulTwo);
 
 // Array of strings
 var names = ['Tim', 'Nick', 'Grant'];
 
 // Write an introduction function that takes an argument and returns "hello, I'm " + argument
+var introFunc = function(arg) {
+	return "hello, I'm " + arg;
+}
 
 
-// Create an array of introductions for each name
 
 // Array of objects
 var people = [
@@ -27,5 +38,18 @@ var people = [
 	{name:'Isabel', age:43}
 ];
 
+// Create an array of introductions for each name
+var intros = [];
+for (var i = 0; i < people.length; i++) {
+	intros.push("My name is " + people[i]['name'] +" and I'm " + people[i]['age'] +" years old");
+}
+console.log(intros);
+
 // Use the `.forEach` method to create a new property `intro` for each object in your people array
 // The property should say "My name is NAME and I'm AGE years old"
+people.forEach(function(object) {
+	object['intro'] = "My name is " + object.name +" and I'm " + object.age +" years old";
+});
+
+console.log(people);
+
